572eafbb61909e5a058c4b11daafce74667a6161
Zakaria Abdullahi <zakariaabdullahi@Zakarias-MacBook-Pro.local>
2024-02-10 00:40:31 -0800

    user routes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
-from app.models import db, User
+from app.models import db, User, To_Do, To_See, To_Eat, Review
 
 
 user_routes = Blueprint('users', __name__)
@@ -24,3 +24,36 @@ def user(id):
     if user is None:
         return jsonify({'Error': "User couldn't be found"})
     return user.to_dict()
+
+# Get to-do's by current user
+@user_routes.route('/to-do')
+def get_user_todo():
+    curr_user_id = current_user.id
+    to_dos = db.session.query(To_Do).filter(
+        To_Do.user_id == curr_user_id).all()
+    return {"To-Do's": [to_do.to_dict() for to_do in to_dos]}
+
+# Get to-see's by current user
+@user_routes.route('/to-see')
+def get_user_tosee():
+    curr_user_id = current_user.id
+    to_sees = db.session.query(To_See).filter(
+        To_See.user_id == curr_user_id).all()
+    return {"To-See's": [to_see.to_dict() for to_see in to_sees]}
+
+# Get to-eat's by current user
+@user_routes.route('/to-eat')
+def get_user_toeat():
+    curr_user_id = current_user.id
+    to_eats = db.session.query(To_Eat).filter(
+        To_Eat.user_id == curr_user_id).all()
+    return {"To-Eat's": [to_eat.to_dict() for to_eat in to_eats]}
+
+
+# Get reviews by current user
+@user_routes.route('/reviews')
+def get_user_reviews():
+    curr_user_id = current_user.id
+    reviews = db.session.query(Review).filter(
+        Review.user_id == curr_user_id).all()
+    return {"My Reviews": [review.to_dict() for review in reviews]}
